657fa43786a29792f1b358265baf1efd0e07e576
kkistya <kikistya@gmail.com>
2024-06-01 18:20:34 +0300

    cleanup

--- crawler_sqlite.py
+++ crawler_sqlite.py
@@ -2,43 +2,42 @@ from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Create an engine that stores data in the local directory's sqlite.db file
 engine = create_engine('sqlite:///data/pages.db', echo=True)
 
-# Declare a Base object for your classes to inherit from
 Base = declarative_base()
 
-
-# Define a User class which will be mapped to a users table
 class Page(Base):
     __tablename__ = 'pages'
     id = Column(Integer, Sequence('page_id'), primary_key=True)
     link = Column(String(200))
+    html = Column(String)
 
     def __repr__(self):
-        return f"<Page(id={self.id}, name={self.link})>"
+        return f"<Page(id={self.id}, link={self.link})>"
 
 
-# Create all tables by issuing CREATE TABLE commands to the DB
 Base.metadata.create_all(engine)
 
-# Create a configured "Session" class
-Session = sessionmaker(bind=engine)
 
-# Create a Session
+Session = sessionmaker(bind=engine)
 session = Session()
 
-# Create new users
-page1 = Page(link='abcde')
-page2 = Page(link='sldgfhaedkvhadk')
 
-# Add the new users to the session
-session.add(page1)
-session.add(page2)
+def add_page(link: str, html: str):
+    existing_page = session.query(Page).filter_by(link=link).first()
+    if existing_page is None:
+        new_page = Page(link=link, html=html)
+        session.add(new_page)
+        session.commit()
+        return True
+    return False
 
-# Commit the session to the database
-session.commit()
 
-# Query the database
+# print(add_page("akfdjvksdfhaefgs", "html_lsghdkghdskg"))
+# print(add_page("skjfskufhvakfdjvksdfhaefgs", "html_lsghdkghdskskgfhskufh usfvhg"))
+# print(add_page("sskkjrfhkjfskufhvakfdjvksdfhaefgs", "html_lsghdkghdskskgfhskufh usfvhg"))
+
 for page in session.query(Page).all():
     print(page)
+
+session.close()
